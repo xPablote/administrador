756071afcf396a9d6b1f7e815fd215c16ec5f38f
Pablo Villanea <pvillanea@gmail.com>
2024-03-30 23:20:06 -0300

    primer commit
    primer commit

---
+++ src/main/java/cl/lafabrica/administrador/GestorAdministrativoDeLaFabricaApplication.java
@@ -0,0 +1,13 @@
+package cl.lafabrica.administrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorAdministrativoDeLaFabricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorAdministrativoDeLaFabricaApplication.class, args);
+	}
+
+}
