4b7ec1b0725287f72b864803176ff507b04d677a
Pablo Villanea <pvillanea@gmail.com>
2024-05-24 01:42:04 -0400

    formateo de fecha

--- src/main/java/cl/lafabrica/administrador/model/Pago.java
+++ src/main/java/cl/lafabrica/administrador/model/Pago.java
@@ -1,11 +1,13 @@
 package cl.lafabrica.administrador.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
+import java.util.Date;
 
 @Data
 @Builder
@@ -17,6 +19,7 @@ public class Pago {
     private String apellidoUsuario;
     private String idPlan;
     private String nombrePlan;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Timestamp fechaPago;
     private Long monto;
     private String metodoPago;
